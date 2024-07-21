875cf363f1123c9e40fe618d8498d2a2d9f36a5a
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-21 15:23:28 +0300

    signup, login, logout route larini yaratish

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   // root route http://localhost:5000/
   res.send("Hello World!");
-})
+});
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.get("/api/auth/signup", (req, res) => {
+  console.log("signup route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  console.log("login route");
+});
+
+app.get("/api/auth/siglogout", (req, res) => {
+  console.log("signup logout");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
