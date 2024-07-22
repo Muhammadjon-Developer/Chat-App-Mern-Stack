d42f058ec3727631c302ef1a244cbb5b2690db7c
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-22 13:00:35 +0300

    signUp endpoint ni yaratish + parolni heshlash vs mongoDb ga saqlash

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,18 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Hello World!");
-});
+dotenv.config();
 
+app.use(express.json()); // JSON payloadlar bilan kiruvchi so'rovlarni tahlil qilish (req.body dan)
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Hello World!");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
