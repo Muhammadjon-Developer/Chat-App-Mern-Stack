613bace7401ca37dfb6f06a776f7ae0784ff0bd1
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-22 19:01:07 +0300

    message model yaratish

---
+++ backend/models/message.mode.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      reuired: true,
+    },
+    // createdAt, updatedAt => message.createdAt : 19:00
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
