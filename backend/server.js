040ba017f9610ab39788ef01e263f7efcb89095e
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-23 16:04:58 +0300

    protect route middleware = (o'ta dasturi)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,10 +11,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // JSON payloadlar bilan kiruvchi so'rovlarni tahlil qilish (req.body dan)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5000/
