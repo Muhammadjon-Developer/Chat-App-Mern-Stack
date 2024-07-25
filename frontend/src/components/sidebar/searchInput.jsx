cc8dc0fbb56b89f1bd72ea3d5140a24fbe15f120
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-25 20:53:40 +0300

    sidebar uchun userlarning ko'rinishini yaratish

--- frontend/src/components/sidebar/searchInput.jsx
+++ frontend/src/components/sidebar/searchInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FaSearch } from "react-icons/fa";
+import { IoSearchSharp } from "react-icons/io5";
 
 const SearchInput = () => {
   return (
     <form className='flex items-center gap-2 '>
       <input type="text" placeholder='Search...' className='input input-bordered rounded-full'/>
       <button type="submit" className='btn btn-circle bg-sky-500 text-white'>
-        <FaSearch />
+        <IoSearchSharp className='w-5 h-5 outline-none' />
       </button>
     </form>
   )
--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import SearchInput from './searchInput';
+import Conversations from "./conversations";
 
 const Sidebar = () => {
   return (
     <div>
       <SearchInput />
       <div className="divider px-3"></div>
-      {/* <Conversations/>
-      <LogoutButton/> */}
+      <Conversations/>
+      {/* <LogoutButton/> */}
     </div>
   );
 };
