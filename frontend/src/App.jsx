435b6352f2c52e4e93db0c425b8d4a11aea4a1a4
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-25 16:30:51 +0300

    signUp page ni yaratish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Login from "./pages/login/login";
+// import Login from "./pages/login/login";
+import SignUp from './pages/signup/signUp';
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center"> 
-     <Login/>
+     <SignUp/>
     </div>
   );
 };
