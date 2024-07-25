7067148f14e38a463c54e04862f87d49f795545c
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-25 21:28:20 +0300

    messege lar jo'natish

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -15,3 +15,22 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+// BOSHLANG'ICH KOD
+// import React from "react";
+// import SearchInput from './searchInput';
+// import Conversations from "./conversations";
+// import LogoutButton from "./logoutButton";
+
+// const Sidebar = () => {
+//   return (
+//     <div className="border-r border-slate-500 p-4 flex flex-col ">
+//       <SearchInput />
+//       <div className="divider px-3"></div>
+//       <Conversations/>
+//       <LogoutButton/>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
