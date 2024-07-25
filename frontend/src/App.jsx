40a880bc072f02ad433aec1c862430eae844c024
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-25 20:01:47 +0300

    react icons kutubxonasini o'ranatish va ikonka qo'yish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Home from "./pages/home/home";
 // import Login from "./pages/login/login";
-import SignUp from './pages/signup/signUp';
+// import SignUp from './pages/signup/signUp';
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center"> 
-     <SignUp/>
+     {/* <SignUp/> */}
+     <Home/>
     </div>
   );
 };
