f6c205e5b20866902b72019aea45fc6445df363b
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-28 15:24:48 +0300

    react router dom route larni yaratish va port ni o'zgartirish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Home from "./pages/home/home";
+import Login from './pages/login/login'
+import SignUp from './pages/signup/signUp'
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="p-4 h-screen flex items-center justify-center"> 
-     <Home/>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 };
 
 export default App;
-
